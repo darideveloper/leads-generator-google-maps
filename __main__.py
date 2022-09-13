017e17aaf38ee820c849a92ab82231749d7f3c53
daridev <hernandezdarifrancisco@gmail.com>
2022-09-12 19:24:02 -0500

    functions docs

--- __main__.py
+++ __main__.py
@@ -16,25 +16,46 @@ class MapsScraper (Web_scraping):
         super().__init__ (headless= not show_browser)
 
     def __search__ (self):
+        """ Open google maps search results """
 
         # Generate maps url
         search_query = f"{self.keywords}+{self.city}".replace(" ", "+")
-        search_page = "https://www.google.com/maps/search/{search_query}/"
+        search_page = f"https://www.google.com/maps/search/{search_query}/"
 
         self.set_page (search_page)
         sleep (2)
 
+    def __check_next__ (self):
+        """ Validate if the current results screen have more pages
+
+        Returns:
+            bool: True, if more pages in the screen
+        """
+        return True
+
     def __go_next__ (self):
+        """ Move to next resuslts page """
         pass
 
     def __extract__ (self):
-        pass
+        """ Get all data in google maps, for the current results screen
 
-    def __send_google_sheets__ (self):
+        Returns:
+            list: list nested with the google maps data
+        """
+        return []
+
+    def __send_google_sheets__ (self, data:list):
+        """ Submit data to google sheet 
+
+        Args:
+            data (list): list nested with the google maps data
+        """
         pass
 
     def auto_run (self):
-        pass
+        """ workflow of the scraper """
+        self.__search__ ()
 
 
 
@@ -53,7 +74,7 @@ def main ():
     maps = MapsScraper (keywords, city, max_results, get_emails, show_browser)
     maps.auto_run ()
 
-    input ("end?")
+    print ()
 
 
 
