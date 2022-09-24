653e05a4c955eb8ecc47da3593e8683fffbb6c8e
daridev <hernandezdarifrancisco@gmail.com>
2022-09-24 13:45:26 -0500

    loading page and run.py

---
+++ run.py
@@ -0,0 +1,22 @@
+import os
+import webbrowser
+from threading import Thread
+from app import app
+
+def open_page ():
+    current_folder = os.path.dirname(__file__)
+    loading_path = os.path.join(current_folder, 'templates', 'loading.html')
+    webbrowser.open (loading_path)
+    print ("openning page")
+    
+
+if __name__ == "__main__":
+    # Create threads
+    thread_open_page = Thread (target=open_page)
+    
+    # Run threads
+    thread_open_page.start ()
+    
+    # Run server without debug
+    app.run (host="0.0.0.0")
+    
