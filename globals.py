05cd2a34f6e14d1adb7a71e8ec7c7fae0350ac61
daridev <hernandezdarifrancisco@gmail.com>
2022-09-26 09:30:04 -0500

    fix header in global data

--- globals.py
+++ globals.py
@@ -2,5 +2,5 @@
 global data
 global status
 
-data = ["Link", "keywords", "Cities", "Name", "Reviews num", "Reviews note", "Category", "Location", "Details", "Web page", "Emails"]
+data = [["Link", "keywords", "Cities", "Name", "Reviews num", "Reviews note", "Category", "Location", "Details", "Web page", "Emails"]]
 status = "running"
